e6569e886f80dd8bc8c776b00c420e7fc94d648c
nerjav <recursosnerjav@gmail.com>
2023-06-26 09:52:22 -0400

    mejoras en el front sidebar y logo

--- resources/views/admin/layout/logo.blade.php
+++ resources/views/admin/layout/logo.blade.php
@@ -1,7 +1,13 @@
 <a href="{{ url('admin') }}" class="navbar-brand">
     {{-- You may use plain text as a logo instead of image --}}
-<img src="{{ asset('images/gestorpass.png')}}"class="img-fluid">
+<img src="{{ asset('images/gestorpass.png')}}" class="gestorpass"><style>
 
-    {{--Text Logo--}}
+.app-header.navbar .navbar-brand img {
+    max-width: 190px;
+    max-height: 46px;
+}
+
+</style>
+
 
 </a>
--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -3,20 +3,23 @@
         <ul class="nav">
            @if (Auth::user()->rol->role_id == 2)
            <li class="nav-title">{{ trans('brackets/admin-ui::admin.sidebar.content') }}</li>
-           <li class="nav-item"><a class="nav-link" href="{{ url('admin/credenciales/inicio') }}"><i class="nav-icon icon-magnet"></i> INICIO</a></li>
+           <li class="nav-item"><a class="nav-link" href="{{ url('admin/credenciales/inicio') }}"><i class="nav-icon icon-magnet"></i>Tablero</a></li>
            <li class="nav-item"><a class="nav-link" href="{{ url('admin/tipodeconexions') }}"><i class="nav-icon icon-drop"></i> {{ trans('admin.tipodeconexion.title') }}</a></li>
            <li class="nav-item"><a class="nav-link" href="{{ url('admin/estados') }}"><i class="nav-icon icon-book-open"></i> {{ trans('admin.estado.title') }}</a></li>
            <li class="nav-item"><a class="nav-link" href="{{ url('admin/servidors') }}"><i class="nav-icon icon-energy"></i> {{ trans('admin.servidor.title') }}</a></li>
            {{-- <li class="nav-item"><a class="nav-link" href="{{ url('admin/cat-informaciones') }}"><i class="nav-icon icon-ghost"></i> {{ trans('admin.cat-informacione.title') }}</a></li> --}}
            <li class="nav-item"><a class="nav-link" href="{{ url('admin/grupos') }}"><i class="nav-icon icon-compass"></i> {{ trans('admin.grupo.title') }}</a></li>
            <li class="nav-item"><a class="nav-link" href="{{ url('admin/tipo-debds') }}"><i class="nav-icon icon-flag"></i> {{ trans('admin.tipo-debd.title') }}</a></li>
-           <li class="nav-item"><a class="nav-link" href="{{ url('admin/verifications') }}"><i class="nav-icon icon-energy"></i> {{ trans('admin.verification.title') }}</a></li>
-           <li class="nav-item"><a class="nav-link" href="{{ url('admin/roleusers') }}"><i class="nav-icon icon-flag"></i> {{ trans('admin.roleuser.title') }}</a></li>
-           <li class="nav-item"><a class="nav-link" href="{{ url('admin/roles') }}"><i class="nav-icon icon-umbrella"></i> {{ trans('admin.role.title') }}</a></li>
            <li class="nav-item"><a class="nav-link" href="{{ url('admin/tipo-servicios') }}"><i class="nav-icon icon-compass"></i> {{ trans('admin.tipo-servicio.title') }}</a></li>
            {{-- Do not delete me :) I'm used for auto-generation menu items --}}
            <li class="nav-title">{{ trans('brackets/admin-ui::admin.sidebar.settings') }}</li>
            <li class="nav-item"><a class="nav-link" href="{{ url('admin/admin-users') }}"><i class="nav-icon icon-user"></i> {{ __('Manage access') }}</a></li>
+                      <li class="nav-item"><a class="nav-link" href="{{ url('admin/roles') }}"><i class="nav-icon icon-umbrella"></i> {{ trans('admin.role.title') }}</a></li>
+
+                      <li class="nav-item"><a class="nav-link" href="{{ url('admin/roleusers') }}"><i class="nav-icon icon-flag"></i> {{ trans('admin.roleuser.title') }}</a></li>
+
+            <li class="nav-item"><a class="nav-link" href="{{ url('admin/verifications') }}"><i class="nav-icon icon-energy"></i> {{ trans('admin.verification.title') }}</a></li>
+
            <li class="nav-item"><a class="nav-link" href="{{ url('admin/translations') }}"><i class="nav-icon icon-location-pin"></i> {{ __('Translations') }}</a></li>
             {{-- Do not delete me :) I'm also used for auto-generation menu items --}}
             {{--<li class="nav-item"><a class="nav-link" href="{{ url('admin/configuration') }}"><i class="nav-icon icon-settings"></i> {{ __('Configuration') }}</a></li>--}}
@@ -24,7 +27,7 @@
 
             @if (Auth::user()->rol->role_id == 1)
            <li class="nav-title">{{ trans('brackets/admin-ui::admin.sidebar.content') }}</li>
-           <li class="nav-item"><a class="nav-link" href="{{ url('admin/credenciales/inicio') }}"><i class="nav-icon icon-magnet"></i> Inicio</a></li>
+           <li class="nav-item"><a class="nav-link" href="{{ url('admin/credenciales/inicio') }}"><i class="nav-icon icon-magnet"></i>Tablero s </a></li>
            <li class="nav-item"><a class="nav-link" href="{{ url('admin/tipodeconexions') }}"><i class="nav-icon icon-drop"></i> {{ trans('admin.tipodeconexion.title') }}</a></li>
            <li class="nav-item"><a class="nav-link" href="{{ url('admin/estados') }}"><i class="nav-icon icon-book-open"></i> {{ trans('admin.estado.title') }}</a></li>
            <li class="nav-item"><a class="nav-link" href="{{ url('admin/servidors') }}"><i class="nav-icon icon-energy"></i> {{ trans('admin.servidor.title') }}</a></li>
